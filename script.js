7eaef4bb324329239eaa7a6d124d0f581b1cf91e
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-13 15:25:09 +0300

    api variables declared

---
+++ script.js
@@ -0,0 +1,5 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=80ddc3815c3830bb99001c4b9cb4d5b1&page=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=80ddc3815c3830bb99001c4b9cb4d5b1&query="'
+
