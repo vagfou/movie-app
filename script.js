50c44a7708655456fe5df925220bea3935cea087
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-13 15:43:06 +0300

    search implemented

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=80ddc3815c3830bb99001c4b9cb4d5b1&page=1'
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=80ddc3815c3830bb99001c4b9cb4d5b1&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=80ddc3815c3830bb99001c4b9cb4d5b1&query=" '
 
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
